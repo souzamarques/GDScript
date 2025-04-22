7ee1c6a3c55a17cc37c01b88fde81442642a8132
Lucas Souza <lucassouzzamarques@gmail.com>
2025-04-22 15:29:49 -0300

    Variables 1.0, if-statements and comments

--- main.gd
+++ main.gd
@@ -1,12 +1,23 @@
 extends Node
 
-func _ready():
-	$Label.text = "Hello, World!"
-	$Label.modulate = Color.GREEN
+var health = 100
+var game_over = false
 
 func _input(event):
-	if event.is_action_pressed("my_action"):
-		$Label.modulate = Color.RED
+	# Test test test
+	if event.is_action_pressed("my_action") and not game_over:
+		health -= 10
+		if(health <= 0):
+			print(health)
+			print("YOU DIED!")
+			print("GAME OVER")
+			game_over = true
+		elif(health == 30):
+			print("You are injured!")
+			print(health)
+		elif(health == 10):
+			print("You are in really bad shape!")
+			print(health)
+		else:
+			print(health)
 	
-	if event.is_action_released("my_action"):
-		$Label.modulate = Color.GREEN
