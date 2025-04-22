2e019b0555b58de011c7d08a8884fc2c28c46475
Lucas Souza <lucassouzzamarques@gmail.com>
2025-04-22 02:54:35 -0300

    Input

--- main.gd
+++ main.gd
@@ -3,3 +3,10 @@ extends Node
 func _ready():
 	$Label.text = "Hello, World!"
 	$Label.modulate = Color.GREEN
+
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.RED
+	
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.GREEN
