337e0638968e3d6dbad83ac43f0d92d3dc5ec4fe
Lucas Souza <lucassouzzamarques@gmail.com>
2025-04-25 01:50:19 -0300

    Dictionaries

--- main.gd
+++ main.gd
@@ -1,19 +1,33 @@
 extends Node
 
 func _ready():
-	var item = ["Potion", 3, 6]
-	var shields: Array[String] = ["Leather Shield", "Wooden Shield", "Iron Shield", "Bronze Shield"] # Array with specified type
-	print(item[0])
-	print(shields)
+	var enemy_dict = {
+		"Wild Dog": 1, 
+		"Wolf": 3,
+		"Bandit": 5, 
+		"Orc": 10,
+		"Troll": 15, 
+		"Cursed Ghost": 20,
+		"Witch": 25,
+		"Fallen Warrior": 30,
+		"Dark Mage": 35,
+		"Death Knight": 40, 
+		"Boss": 55,
+	}
 	
-	shields.remove_at(3)
-	shields.append("Dwarven Shield")
-	print("Shields: " + str(shields))
-
-	for i in item:
-		print(i)
-		
-	var glass := 0.0
-	while glass < 0.5:
-		glass += randf_range(0.1, 0.2)
-		print(glass)
+	print("Fallen Warrior level: " + str(enemy_dict["Fallen Warrior"]))
+	
+	enemy_dict["Demon King"] = 70
+	enemy_dict["Final Boss"] = 90
+	
+	for i in enemy_dict:
+		print("Character: " + i + ". Level: " + str(enemy_dict[i]))
+	
+	# dictionaries inside another dictionary
+	var party_dict = {
+		"Hero":		{"Level": 1, "Health": 20},
+		"Warrior":	{"Level": 1, "Health": 25},
+		"Cleric":	{"Level": 1, "Health": 15},
+		"Mage":		{"Level": 1, "Health": 10},
+	}
+	print("Cleric health: " + str(party_dict["Cleric"]["Health"]))
