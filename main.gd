09d27fafeca11738466c9ed82212a3c36b102ff1
Lucas Souza <lucassouzzamarques@gmail.com>
2025-04-25 14:06:46 -0300

    Enums and match

--- main.gd
+++ main.gd
@@ -1,33 +1,40 @@
 extends Node
 
+enum Alignment { ALLY, NEUTRAL, ENEMY } # enum has default values for the constants, but it is possible to set values. For example: enum Alignment { ALLY = 1, NEUTRAL = 2, ENEMY = 3 }
+enum Destruction_Spells { FIRE, FROST, THUNDER }
+enum Illusion_Spells { FURY, PACIFY, FEAR, MUFFLE, INVISIBILITY}
+# Godot is creating a constant for each state in the enum. As in the code below for example:
+# const ALLY = 0
+# const NEUTRAL = 1
+# const ENEMY = 2 /// This code is completely identical to the thrid and fourth lines
+
+var unit_alignment = Alignment.ALLY
+@export var mage_spells : Destruction_Spells
+@export var mage_illusion : Illusion_Spells
+
 func _ready():
-	var enemy_dict = {
-		"Wild Dog": 1, 
-		"Wolf": 3,
-		"Bandit": 5, 
-		"Orc": 10,
-		"Troll": 15, 
-		"Cursed Ghost": 20,
-		"Witch": 25,
-		"Fallen Warrior": 30,
-		"Dark Mage": 35,
-		"Death Knight": 40, 
-		"Boss": 55,
-	}
-	
-	print("Fallen Warrior level: " + str(enemy_dict["Fallen Warrior"]))
-	
-	enemy_dict["Demon King"] = 70
-	enemy_dict["Final Boss"] = 90
-	
-	for i in enemy_dict:
-		print("Character: " + i + ". Level: " + str(enemy_dict[i]))
-	
-	# dictionaries inside another dictionary
-	var party_dict = {
-		"Hero":		{"Level": 1, "Health": 20},
-		"Warrior":	{"Level": 1, "Health": 25},
-		"Cleric":	{"Level": 1, "Health": 15},
-		"Mage":		{"Level": 1, "Health": 10},
-	}
-	print("Cleric health: " + str(party_dict["Cleric"]["Health"]))
+	if unit_alignment == Alignment.ENEMY:
+		print("You are not welcome here!")
+	else:
+		print("Welcome!")
+		
+	if mage_spells == Destruction_Spells.FIRE:
+		print("You are a skilled practitioner of fire magic.")
+	elif mage_spells == Destruction_Spells.FROST:
+		print("You are a skilled practitioner of frost magic.")
+	else:
+		print("You are a skilled practitioner of thunder magic.")
+		
+	match mage_illusion:
+		Illusion_Spells.FURY:
+			print("You have cast a Fury Spell")
+		Illusion_Spells.PACIFY:
+			print("You have cast a Pacify Spell")
+		Illusion_Spells.FEAR:
+			print("You have cast a Fear Spell")
+		Illusion_Spells.MUFFLE:
+			print("You have cast a Muffle Spell")
+		Illusion_Spells.INVISIBILITY:
+			print("You have cast an invisibility spell")
+		_:
+			print("You don't have enough mana")
