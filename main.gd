7006daeede8d7867330b081792afc55a560d1352
Lucas Souza <lucassouzzamarques@gmail.com>
2025-04-26 20:49:27 -0300

    Signals

--- main.gd
+++ main.gd
@@ -1,9 +1,23 @@
 extends Node
 
-@onready var weapon: Node = $Player/Weapon
-@export var node_enemy : Node # assigning reference to node Enemy
-@export var player_graphics : Sprite2D # can only accept Sprites nodes
+signal leveled_up(msg)
+
+var xp := 0
 
 func _ready():
-	if weapon is Node:
-		print("Node")
+	leveled_up.connect(_on_leveled_up)
+
+# func _on_button_pressed() -> void:
+#	print("Test test")
+
+func _on_timer_timeout() -> void:
+	xp += 5
+	print(xp)
+	if xp > 0 and xp < 20:
+		print("Test reached a new level!")
+	else:
+		xp = 0
+		leveled_up.emit("Nice!")
+
+func _on_leveled_up(msg):
+	print(msg)
